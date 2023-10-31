d423b456fb0d75884ac73fbfa3be8c2c692d3c3f
ayushk-1801 <ayush23162@iiitd.ac.in>
2023-10-31 22:56:48 +0530

    Update Number_Theory_Templates.cpp

--- Number_Theory_Templates.cpp
+++ Number_Theory_Templates.cpp
@@ -29,6 +29,24 @@ void sieve(int n) {
     }
 }
 
+// Prime Factorization
+void primeFactorization(int n) {
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            int count = 0;
+            while (n % i == 0) {
+                count++;
+                n /= i;
+            }
+            cout << i << "^" << count << endl;
+        }
+    }
+
+    if (n > 1) {
+        cout << n << "^" << 1 << endl;
+    }
+}
+
 int main() {
     FAST_IO
     return 0;
