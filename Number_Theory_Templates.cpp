e5c83ac09bebb3eb03c43518af64a3beb44e9e93
ayushk-1801 <ayush23162@iiitd.ac.in>
2023-11-02 20:15:08 +0530

    Update Number_Theory_Templates.cpp

--- Number_Theory_Templates.cpp
+++ Number_Theory_Templates.cpp
@@ -47,6 +47,35 @@ void primeFactorization(int n) {
     }
 }
 
+// Binary Exponentiation
+int power_binaryExponentiation(int a, int n) {
+    int res = 1;
+    while (n) {
+        if (n % 2) {
+            res *= a;
+            n--;
+        } else {
+            a *= a;
+            n /= 2;
+        }
+    }
+    return res;
+}
+
+// Modular Exponentiation
+int power_modularExponentiation(int a, int n, int p) {
+    int res = 1;
+    while (n) {
+        if (n % 2) {
+            res = (res * a) % p;
+            n--;
+        } else {
+            a = (a * a) % p;
+            n /= 2;
+        }
+    }
+}
+
 int main() {
     FAST_IO
     return 0;
