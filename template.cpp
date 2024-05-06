80949f8590dc56ebddc4103004dfd5b120e79f16
ayushk-1801 <ayush23162@iiitd.ac.in>
2024-05-06 19:58:39 +0530

    First commit

--- template.cpp
+++ template.cpp
@@ -1,20 +1,38 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-#define FAST_IO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 
-#ifndef ONLINE_JUDGE
-#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+#ifdef ayush_1801
+#define debug(x) cerr << #x<<" "; _print(x); cerr << endl;
 #else
-#define debug(x)
+#define debug(x);
 #endif
 
-void _print(long long t) {cerr << t;}
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+void _print(ll t) {cerr << t;}
 void _print(int t) {cerr << t;}
 void _print(string t) {cerr << t;}
 void _print(char t) {cerr << t;}
-void _print(long double t) {cerr << t;}
+void _print(lld t) {cerr << t;}
 void _print(double t) {cerr << t;}
-void _print(unsigned long long t) {cerr << t;}
+void _print(ull t) {cerr << t;}
 
 template <class T, class V> void _print(pair <T, V> p);
 template <class T> void _print(vector <T> v);
@@ -27,18 +45,16 @@ template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i
 template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
 template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
 
+/*--------------------------------------------------------------------------------------------------------------------------*/
 
 void solve() {
 
 }
 
 int main() {
-#ifndef ONLINE_JUDGE
-	freopen("errorf.err", "w", stderr);
-#endif
-
-	FAST_IO
-	int TC = 1;
-	cin >> TC;
-	while (TC--) solve();
+    fastio();
+    int TC = 1;
+    cin >> TC;
+    while(TC--)
+        solve();
 }
