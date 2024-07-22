acea86b0013962d8dd99f05962a466a158391a59
ayushk-1801 <ayush23162@iiitd.ac.in>
2024-07-22 20:46:25 +0530

    Added Modular Exponentiation

--- Number Theory/basic.cpp
+++ Number Theory/basic.cpp
@@ -1,25 +1,42 @@
 /* SIEVE OF ERATOSTHENES */
-int N;
-vector<bool> primes(N + 1, true);
-
-void sieve() {
-    primes[0] = primes[1] = false;
-    for (int i = 2; i <= N; i++) {
-        if (primes[i] && (long long)i * i <= N) {
-            for (int j = i * i; j <= N; j += i)
-                primes[j] = false;
+vector<ll> sieve(int n) {
+    int *arr = new int[n + 1]();
+    vector<ll> vect;
+    for (int i = 2; i <= n; i++)
+        if (arr[i] == 0) {
+            vect.push_back(i);
+            for (int j = 2 * i; j <= n; j += i)
+                arr[j] = 1;
         }
-    }
+    return vect;
 }
 
 /* BINARY EXPONENTIATION */
-long long binExpo(long long a, long long b) {
-    long long res = 1;
+ll expo(ll a, ll b, ll mod) {
+    ll res = 1;
     while (b > 0) {
         if (b & 1)
-            res = res * a;
-        a = a * a;
-        b >>= 1;
+            res = (res * a) % mod;
+        a = (a * a) % mod;
+        b = b >> 1;
     }
     return res;
 }
+
+/* MODULAR ARITHMETIC */
+ll mod_add(ll a, ll b, ll m) {
+    a = a % m;
+    b = b % m;
+    return (((a + b) % m) + m) % m;
+}
+ll mod_mul(ll a, ll b, ll m) {
+    a = a % m;
+    b = b % m;
+    return (((a * b) % m) + m) % m;
+}
+ll mod_sub(ll a, ll b, ll m) {
+    a = a % m;
+    b = b % m;
+    return (((a - b) % m) + m) % m;
+}
+
