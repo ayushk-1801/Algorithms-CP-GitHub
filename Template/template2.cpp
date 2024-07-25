fe5fea73f16cfb450b0a54b9845af4d9ef9f5af4
ayushk-1801 <ayush23162@iiitd.ac.in>
2024-07-25 12:30:54 +0530

    Added MOD1

--- Template/template2.cpp
+++ Template/template2.cpp
@@ -4,6 +4,7 @@ using namespace chrono;
 
 #define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 #define MOD 1000000007
+#define MOD1 998244353
 #define INF 1e18
 #define nl "\n"
 #define pb push_back
